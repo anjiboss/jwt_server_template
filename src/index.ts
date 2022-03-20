66d13e082728c89e5c3cbac8c697adbb8afbcf4d
Anji <nhu.nhu.face@gmail.com>
2022-03-21 00:05:41 +0900

    update filename

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,21 @@ import "reflect-metadata";
 import express from "express";
 import cors from "cors";
 import { createConnection } from "typeorm";
-import { userRouter } from "./routes/user.route";
-import { authRouter } from "./routes/auth.route";
+import { userRouter } from "./routes/user";
+import { authRouter } from "./routes/auth";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const main = () => {
-  createConnection()
-    .then(async () => {
-      app.use("/api/v1/user", userRouter);
-      app.use("/api/v1/auth", authRouter);
-      app.listen(PORT, () => console.log(PORT))
-    })
-    .catch(error => console.log(error))
-}
+const main = async () => {
+  await createConnection();
+  app.use("/api/v1/user", userRouter);
+  app.use("/api/v1/auth", authRouter);
+  app.listen(PORT, () =>
+    console.log(`Server is listening 🚀 on Port: ${PORT}`)
+  );
+};
 
 main();
